4edf66d66aa66e1d7f51adfd0ae81a9b81b2f9cc
nmsn <136696700@qq.com>
2022-09-01 15:13:17 +0800

    feat(components): add tabs

--- components/Tabs.tsx
+++ components/Tabs.tsx
@@ -1,47 +1,85 @@
 import styled from "styled-components";
+import { useEffect, useState } from "react";
+import Space from "./Space";
+import { mix2Color } from "@nmsn/color-utils";
+import { themeColor, themeColor2 } from "../utils/theme";
 
 type TabValue = string | number;
 
 type TabType = { label: string; value: TabValue };
 
+const hoverColor = mix2Color([themeColor2, "white"], "hex", [0.8, 0.2]);
+
 const StyledTab = styled.div`
-  padding: 8 10px;
-  color: red;
-  background-color: blue;
+  padding: 8px 10px;
+  color: white;
+  background-color: ${(props) => (props.isActive ? themeColor : themeColor2)};
+  width: 100px;
+  border-radius: 4px;
+
+  ${(props) =>
+    props.isActive
+      ? `cursor: none;`
+      : `:hover {
+    background-color: ${hoverColor};
+    cursor: pointer;
+  }`}
 `;
 
 const Tab = ({
   isActive,
   label,
   value,
+  onChange,
 }: {
   isActive: boolean;
   label: string;
   value: TabValue;
+  onChange: (val: TabValue) => void;
 }) => {
-  return <StyledTab>{label}</StyledTab>;
+  return (
+    <StyledTab
+      isActive={isActive}
+      onClick={!isActive ? () => onChange(value) : null}
+    >
+      {label}
+    </StyledTab>
+  );
 };
 
 const Tabs = ({
   onChange,
-  activeKey = 1,
+  activeTab = 1,
   tabs,
 }: {
-  onChange?: () => void;
-  activeKey?: string | number;
+  onChange?: (val: TabValue) => void;
+  activeTab?: string | number;
   tabs: TabType[];
 }) => {
+  const [curKey, setCurKey] = useState<TabValue>(activeTab);
+
+  useEffect(() => {
+    setCurKey(activeTab);
+  }, [activeTab]);
+
+  const onCurChange = (val: TabValue) => {
+    console.log(val);
+    setCurKey(val);
+    onChange?.(val);
+  };
+
   return (
-    <div>
+    <Space type="vertical">
       {tabs.map(({ label, value }) => (
         <Tab
           key={value}
           label={label}
           value={value}
-          isActive={value === activeKey}
+          isActive={value === curKey}
+          onChange={onCurChange}
         />
       ))}
-    </div>
+    </Space>
   );
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,25 @@
-import React, { useState } from "react";
-import { ChromePicker, ColorResult } from "react-color";
-import { model2Color, mix2Color } from "@nmsn/color-utils";
-import ColorContrast from "../components/ColorContrast";
-import ColorMix from '../components/ColorMix';
-
-import styles from "./index.module.css";
+import { useState } from "react";
+import Layout from "../components/Layout";
+import ColorPickerGroup from "../components/ColorPickerGroup";
+import Tabs from "../components/Tabs";
 
 const HomePage = () => {
+  const [tab, setTab] =
+    useState<React.ComponentProps<typeof Tabs>["activeTab"]>(1);
+  const onChange = (value) => console.log(value);
+
+  const tabs = [
+    { label: "颜色对比", value: 1 },
+    { label: "颜色融合", value: 2 },
+  ] as React.ComponentProps<typeof Tabs>["tabs"];
   return (
-    <div className={styles.index}>
-      <ColorContrast />
-      <ColorMix />
-    </div>
+    <Layout>
+      <div style={{ marginRight: 20 }}>
+        <Tabs tabs={tabs} onChange={setTab} activeTab={tab} />
+      </div>
+
+      <ColorPickerGroup onChange={onChange} />
+    </Layout>
   );
 };
 
