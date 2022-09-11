0d33ea4b99a9b101916b1acc1363d7f8a7f0033e
nmsn <136696700@qq.com>
2022-09-11 15:05:35 +0800

    feat: update homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,100 @@
 import { useState } from "react";
+import {
+  model2Color,
+  mix2Color,
+  calcComplementaryColor,
+} from "@nmsn/color-utils";
 import Layout from "../components/Layout";
+import ColorPicker from "../components/ColorPicker";
 import ColorPickerGroup from "../components/ColorPickerGroup";
+// import  from ''
+import ColorCardGroup from "../components/ColorCardGroup";
 import Tabs from "../components/Tabs";
+
+const ColorContrast = () => {
+  const onChange = (value) => setColors(value);
+
+  const [colors, setColors] = useState([]);
+
+  const newColors = colors.map((item) => model2Color(item.rgb, "rgb"));
+  return (
+    <>
+      <ColorPickerGroup onChange={onChange} />
+      <div style={{ marginTop: 20 }}>
+        <ColorCardGroup
+          width={220}
+          height={100}
+          colors={newColors}
+          distance={20}
+        />
+      </div>
+    </>
+  );
+};
+
+const ColorMix = () => {
+  const [colors, setColors] = useState([]);
+  const onChange = (value) => setColors(value);
+
+  const newColors = colors.map((item) => model2Color(item.rgb, "rgb"));
+
+  const result = mix2Color(newColors, "rgb");
+
+  console.log(newColors, result);
+
+  return (
+    <>
+      <ColorPickerGroup onChange={onChange} />
+      <div style={{ marginTop: 20 }}>
+        <ColorCardGroup
+          width={220}
+          height={100}
+          colors={newColors}
+          distance={20}
+        />
+      </div>
+      <div style={{ marginTop: 20 }}>
+        <ColorCardGroup
+          width={460}
+          height={100}
+          colors={[result]}
+          distance={20}
+        />
+      </div>
+    </>
+  );
+};
+
+const ColorComplementary = () => {
+  const onChange = (value) => setColor(value);
+  const [color, setColor] = useState({ rgb: { r: 0, g: 0, b: 0, a: 1 } });
+  const newColor = model2Color(color?.rgb, "rgb");
+  const result = calcComplementaryColor(newColor);
+
+  console.log(newColor, result);
+  return (
+    <>
+      <ColorPicker onChange={onChange} />
+      <div style={{ marginTop: 20 }}>
+        <ColorCardGroup
+          width={220}
+          height={100}
+          colors={[newColor, result]}
+          distance={20}
+        />
+      </div>
+    </>
+  );
+};
 
 const HomePage = () => {
   const [tab, setTab] =
     useState<React.ComponentProps<typeof Tabs>["activeTab"]>(1);
-  const onChange = (value) => console.log(value);
 
   const tabs = [
     { label: "颜色对比", value: 1 },
     { label: "颜色融合", value: 2 },
+    { label: "补色", value: 3 },
   ] as React.ComponentProps<typeof Tabs>["tabs"];
   return (
     <Layout>
@@ -18,7 +102,11 @@ const HomePage = () => {
         <Tabs tabs={tabs} onChange={setTab} activeTab={tab} />
       </div>
 
-      <ColorPickerGroup onChange={onChange} />
+      <div>
+        {tab === 1 && <ColorContrast />}
+        {tab === 2 && <ColorMix />}
+        {tab === 3 && <ColorComplementary />}
+      </div>
     </Layout>
   );
 };
