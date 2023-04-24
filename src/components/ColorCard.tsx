2918cd688eb0a240598f14e261cc0c66b3150a3d
nmsn <136696700@qq.com>
2023-04-24 14:34:34 +0800

    feat: 增加剪贴板功能

--- src/components/ColorCard.tsx
+++ src/components/ColorCard.tsx
@@ -1,9 +1,12 @@
 import { useState } from 'react';
+import copy from 'copy-to-clipboard';
 
 import { color2Color, isLight } from '@nmsn/color-utils';
 
 import { textBackgroundColor } from '../utils/theme';
 
+import Check from './Check';
+import Copy from './Copy';
 import Space from './Space';
 
 const Card = ({
@@ -32,6 +35,8 @@ const Card = ({
         justifyContent: 'center',
         alignItems: 'center',
         boxShadow: 'rgb(0 0 0 / 15%) 0px 8px 16px',
+        boxSizing: 'border-box',
+        padding: 12,
       }}
       onMouseEnter={() => setVisible(true)}
       onMouseLeave={() => setVisible(false)}
@@ -41,25 +46,49 @@ const Card = ({
   );
 };
 
-const CardText = ({ children, color }: { children: React.ReactNode; color: string }) => (
-  <div
-    css={{
-      color,
-      backgroundColor: textBackgroundColor,
-      textAlign: 'center',
-      padding: '4px 12px',
-      borderRadius: 4,
-      fontSize: 14,
-      opacity: 0.3,
-      cursor: 'pointer',
-      '&:hover': {
-        opacity: 0.8,
-      },
-    }}
-  >
-    {children}
-  </div>
-);
+const CardText = ({ children, color }: { children: string; color: string }) => {
+  const [visible, setVisible] = useState(false);
+  const [checked, setChecked] = useState(false);
+
+  const onCopy = () => {
+    copy(children);
+    setChecked(true);
+  };
+
+  return (
+    <div
+      css={{
+        color,
+        backgroundColor: textBackgroundColor,
+        textAlign: 'center',
+        padding: '4px 12px',
+        borderRadius: 4,
+        fontSize: 14,
+        opacity: 0.3,
+        cursor: 'pointer',
+        userSelect: 'none',
+        position: 'relative',
+        '&:hover': {
+          opacity: 0.8,
+        },
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+      }}
+      onMouseEnter={() => setVisible(true)}
+      onMouseLeave={() => {
+        if (checked) setChecked(false);
+        setVisible(false);
+      }}
+      onClick={onCopy}
+    >
+      <div css={{ flex: '1 1 auto' }}>{children}</div>
+      <div css={{ paddingLeft: 4, width: 20, height: 20 }}>
+        {visible && (checked ? <Check /> : <Copy />)}
+      </div>
+    </div>
+  );
+};
 
 type ColorCardProps = {
   color: string;
